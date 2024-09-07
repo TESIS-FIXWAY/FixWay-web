a221c2301ce62cfa67e91df33c02491c907cd6e3
fernet_en_mano <91861841+skantos@users.noreply.github.com>
2024-09-06 21:21:34 -0400

    mas diseño

--- FixWay/FixWayApp/urls.py
+++ FixWay/FixWayApp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_view, name='index'),  # Ruta para la página principal
-    path('inventario/', views.inventario_view, name='inventario'),  # Ruta para el inventario
+    path('', views.index_view, name='index'),
+    path('inventario/', views.inventario_view, name='inventario'),
+    path('producto/<str:producto_id>/', views.detalle_producto, name='detalle_producto'),  # Captura el ID como string
 ]
--- FixWay/FixWayApp/views.py
+++ FixWay/FixWayApp/views.py
@@ -1,8 +1,7 @@
-# views.py
-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.conf import settings  # Importa la configuración desde django.conf
 from firebase_admin import firestore
+from django.http import Http404  # Para manejar errores 404
 
 def index_view(request):
     return render(request, 'index.html')  
@@ -17,6 +16,23 @@ def inventario_view(request):
     inventarios = []
     for doc in documentos:
         inventario = doc.to_dict()
+        inventario['id'] = doc.id  # Incluye el ID del documento en los datos
         inventarios.append(inventario)
 
-    return render(request, 'inventario.html', {'inventarios': inventarios})
+    return render(request, 'tienda/inventario.html', {'inventarios': inventarios})
+
+def detalle_producto(request, producto_id):
+    # Obtén el cliente Firestore desde la configuración
+    db = firestore.client()
+
+    # Busca el documento en la colección 'inventario'
+    try:
+        documento = db.collection('inventario').document(producto_id).get()
+        if documento.exists:
+            producto = documento.to_dict()
+        else:
+            raise Http404("Producto no encontrado")
+    except:
+        raise Http404("Error al obtener los detalles del producto")
+
+    return render(request, 'tienda/detalleProducto.html', {'producto': producto})
